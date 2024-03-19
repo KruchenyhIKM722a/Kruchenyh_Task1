b6f8663418384832e32a7c7bb9ad0f8d682a8cb6
Qbox <Qbox@ARTEM>
2024-03-19 20:54:08 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 4, 2, 8, 12, 5 };
+
+        int sum = 0;
+
+        foreach (int element in array)
+        {
+            sum += element;
+        }
+
+        Console.WriteLine("Сума всех элементов в массиве: " + sum);
+    }
+}
